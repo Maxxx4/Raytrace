f8479029018c8b5a01acedf1f112846385a80dea
MaxSergueev <max.sergueev@gmail.com>
2025-01-29 21:42:28 +0100

    [Feat] Added a sphere to the scene
    Also fixed a bug where viewport width was incorrectly calculated, causing sphere to be stretched

--- main.cpp
+++ main.cpp
@@ -3,8 +3,20 @@
 #include "ray.h"
 
 #include <iostream>
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+	vec3 oc = center - r.origin();
+	auto a = dot(r.direction(), r.direction());
+	auto b = -2.0 * dot(r.direction(), oc);
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant >= 0);
+}
+
 
 color ray_color(const ray& r) {
+	if (hit_sphere(point3(0, 0, -1), 0.5, r))
+		return color(1, 0, 0);
+
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - a) * color(0.949, 0.125, 0.929) + a * color(1, 0.514, 0.008);
@@ -21,7 +33,7 @@ int main() {
 	// Camera
 	auto focal_length = 1.0;
 	auto viewport_height = 2.0;
-	auto viewport_width = viewport_height * viewport_height;
+	auto viewport_width = viewport_height * aspect_ratio;
 	auto camera_center = point3(0, 0, 0);
 
 	// Calculate vectors across horizontal and vertical viewport edges
