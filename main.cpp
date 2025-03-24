f43decc624b9eec0e75758d30bcde3517e7510e6
Maxxx4 <max.sergueev@gmail.com>
2025-03-24 23:23:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,34 +9,64 @@
 
 int main() {
 
-	hittable_list scene;
+    hittable_list scene;
 
-	auto material_ground = make_shared<lambertian>(color(0.84, 0.36, 0.69));
-	auto material_left = make_shared<dielectric>(1.0 / 1.33);
-	auto material_middle = make_shared<lambertian>(color(0.0, 0.9, 0.8));
-	auto material_right = make_shared<metal>(color(0.98, 0.97, 0.44), 0.5);
+    auto ground_material = make_shared<lambertian>(color(0.84, 0.36, 0.69));
+    scene.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
-	scene.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-	scene.add(make_shared<sphere>(point3(-1.0, 0, -1.0), 0.5, material_left));
-	scene.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_middle));
-	scene.add(make_shared<sphere>(point3(1, 0.0, -1.0), 0.5, material_right));
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
-	camera cam;
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
 
-	cam.aspect_ratio = 16.0 / 9.0;
-	cam.image_width = 1080;
-	cam.max_depth = 50;
-	cam.samples_per_pixel = 500;
+                if (choose_mat < 0.8) {
+                    // Lambertian
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    scene.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+                else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    scene.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+                else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    scene.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
 
-	cam.vfov = 20;
-	cam.lookfrom = point3(-2, 2, 1);
-	cam.lookat = point3(0, 0, -1);
-	cam.vup = vec3(0, 1, 0);
+    auto material1 = make_shared<dielectric>(1.5);
+    scene.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
 
-	cam.focus_dist = 3.4;
-	cam.defocus_angle = 10.0;
+    auto material2 = make_shared<lambertian>(color(0.0, 0.9, 0.8));
+    scene.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
 
-	cam.render(scene);
+    auto material3 = make_shared<metal>(color(0.98, 0.97, 0.44), 0.0);
+    scene.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
+    camera cam;
 
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 1080;
+    cam.max_depth = 50;
+    cam.samples_per_pixel = 500;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(10, 2, 8);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0.6;
+    cam.focus_dist = 10.0;
+
+    cam.render(scene);
 }
